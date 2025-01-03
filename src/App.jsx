20c7f1759b5fedf315834347fda5be546acc8ab9
boluwatife <sanyaoluboluwatife9@gmail.com>
2025-01-03 16:54:42 +0100

    new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import "primeicons/primeicons.css";
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<>
+			<div className="bg-gray-300">
+				<Header />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<>
+			{/* LOGO */}
+			<div className="flex">
+				<i className="pi pi-user"></i>
+				<p className="">Magnificent<br/>soles</p>
+			</div>
+            {/* NavBar */}
+            <div className="">
+                
+            </div>
+		</>
+	);
+}
